763c7069e817d15e974a66eb15d266d715dca7f7
Owen Henley <owenhenley@me.com>
2018-08-16 11:15:35 -0600

    Recommit to fix issues

--- game/Controllers/ViewControllers/GameViewController.swift
+++ game/Controllers/ViewControllers/GameViewController.swift
@@ -10,21 +10,22 @@ import UIKit
 
 class GameViewController: UIViewController {
 
-    override func viewDidLoad() {
+	@IBOutlet weak var timerLabel: UILabel!
+	@IBOutlet weak var charadeLabel: UILabel!
+	@IBOutlet weak var resultTotalLabel: UILabel!
+	
+	override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+	
+	@IBAction func gotItNextButtonTapped(_ sender: UIButton) {
+	}
+	
+	@IBAction func passButtonTapped(_ sender: UIButton) {
+	}
+	
+	@IBAction func newGameButton(_ sender: UIButton) {
+	}
+	
 }
