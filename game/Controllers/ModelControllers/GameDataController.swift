1c473f921e89c6efc3882aec41c8da4265e0e105
Eric Lanza <eric@lanza.io>
2018-08-16 11:32:49 -0600

    Adds GameDataController functionality
    • Adds shared instance, stored data, newGame function, and increment score function
    • Changes GameData properties to variables

--- game/Controllers/ModelControllers/GameDataController.swift
+++ game/Controllers/ModelControllers/GameDataController.swift
@@ -10,5 +10,20 @@ import Foundation
 
 class GameDataController {
     
+    //MARK: - Shared Instance
+    static let shared = GameDataController()
     
+    //MARK: - Property
+    var gameData: GameData?
+    
+    //MARK: - Helper Functions
+    func startNewGame() {
+        let gameData = GameData(timer: 120, score: 0)
+        self.gameData = gameData
+    }
+    
+    func incrementScore() {
+        guard let gameData = gameData else { print("how did the score not go up?"); return }
+        gameData.score += 1
+    }
 }
