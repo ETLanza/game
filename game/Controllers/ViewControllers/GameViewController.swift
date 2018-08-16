85a66ed3d4a3154ea7fcdd3c00271dabeb1e67e9
Owen Henley <owenhenley@me.com>
2018-08-16 11:00:42 -0600

    Made changes to the storyboard, add View Controller swift files

---
+++ game/Controllers/ViewControllers/GameViewController.swift
@@ -0,0 +1,30 @@
+//
+//  GameViewController.swift
+//  game
+//
+//  Created by Owen Henley on 8/16/18.
+//  Copyright © 2018 ETLanza. All rights reserved.
+//
+
+import UIKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
